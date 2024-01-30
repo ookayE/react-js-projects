284754402ad2766692eee1230684464fbaa93df0
ookayE <smith.evan93@gmail.com>
2024-01-30 00:33:59 -0500

    images, currentSlide, errorMessage, loading states all initialized. Error handling begun. No API implementationy yet.

---
+++ image-slider/src/Components/index.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export default function ImageSlider({ url, limit }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+
+      const response = await fetch(getUrl);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMessage(e.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
+
+  if (loading) {
+    return <div>Loading data...</div>;
+  }
+
+  if (errorMessage !== null) {
+    return <div>Error occurred: {errorMessage}</div>;
+  }
+
+  return <div className="container"></div>;
+}
