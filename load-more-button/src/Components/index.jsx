96a306c68717879cfb751163ed1e71da74b188e1
ookayE <smith.evan93@gmail.com>
2024-02-06 16:36:51 -0500

    Load more button disabled at 100 items. Added comments. index.js changed to jsx

--- load-more-button/src/Components/index.jsx
+++ load-more-button/src/Components/index.jsx
@@ -5,8 +5,9 @@ export default function LoadMoreButton() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [count, setCount] = useState(0);
-  const [disable, setDisable] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
+  // Function to fetch products from an API
   async function fetchProducts() {
     try {
       setLoading(true);
@@ -18,6 +19,7 @@ export default function LoadMoreButton() {
 
       const result = await response.json();
 
+      // Check if there are products in the result and update the state
       if (result && result.products && result.products.length) {
         setProducts((prevData) => [...prevData, ...result.products]);
         setLoading(false);
@@ -30,10 +32,17 @@ export default function LoadMoreButton() {
     }
   }
 
+  //useEffect to fetch products 'count' state changes
   useEffect(() => {
     fetchProducts();
   }, [count]);
 
+  //useEffect to check if there are 100 products and disable the button
+  useEffect(() => {
+    if (products && products.length === 100) setDisabled(true);
+  });
+
+  // If data is loading, display a loading message
   if (loading) {
     return <div>Data is loading. Please wait!</div>;
   }
@@ -41,6 +50,7 @@ export default function LoadMoreButton() {
   return (
     <div className="container">
       <div className="product-container">
+        {/* Display the products if there are any */}
         {products && products.length
           ? products.map((item) => (
               <div className="product" key={item.id}>
@@ -55,11 +65,13 @@ export default function LoadMoreButton() {
           : null}
       </div>
       <div className="button-container">
-        <button disabled={disable} onClick={() => setCount(count + 1)}>
+        {/* Button to load more products, disabled if 'disabled' state is true */}
+        <button disabled={disabled} onClick={() => setCount(count + 1)}>
           Load More Products
         </button>
 
-        {disable ? <p>No more products to show!</p> : null}
+        {/* Display a message if the button is disabled */}
+        {disabled ? <p>No more products to show!</p> : null}
       </div>
     </div>
   );
