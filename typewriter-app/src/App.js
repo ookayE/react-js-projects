a60210cb0ea87749d1be200bf6d549a477a6434d
ookayE <smith.evan93@gmail.com>
2024-03-16 22:16:22 -0400

    typewriter app init. tailwind installed.

---
+++ typewriter-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Typewriter from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Typewriter />
+    </div>
+  );
+}
+
+export default App;
