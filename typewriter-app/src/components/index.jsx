02a180b952a71f329f81b84bc70a8f4d6710e55e
ookayE <smith.evan93@gmail.com>
2024-03-16 22:16:22 -0400

    typewriter app init. tailwind installed.

---
+++ typewriter-app/src/components/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Typewriter() {
+  return (
+    <div>
+      <p className="text-red-700 text-6xl">PLEASE WORK</p>
+    </div>
+  );
+}
+
+export default Typewriter;
