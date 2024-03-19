5d810d90f9e4debc106b71fbf9768b6a961d6a3f
ookayE <smith.evan93@gmail.com>
2024-03-19 17:10:13 -0400

    Housing Marketplace app init. Explore, offers, profile, signup, signin, forgotpassword pages created. Firebase installed.

---
+++ housing-marketplace/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
+function App() {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ housing-marketplace/src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBONY0nhoVqtMcGoUvp5ZXgg6K713qFZiA",
+  authDomain: "housing-marketplace-app-3ecee.firebaseapp.com",
+  projectId: "housing-marketplace-app-3ecee",
+  storageBucket: "housing-marketplace-app-3ecee.appspot.com",
+  messagingSenderId: "177323543575",
+  appId: "1:177323543575:web:df056852fd08e33e932052",
+};
+
+// Initialize Firebase
+export const db = getFirestore;
