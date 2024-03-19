3b390e84859827358e5b5f1aa4520a1c8c279cfe
ookayE <smith.evan93@gmail.com>
2024-03-19 18:13:52 -0400

    Navbar started. React Router Dom imported, routes started.

--- housing-marketplace/src/App.js
+++ housing-marketplace/src/App.js
@@ -8,9 +8,19 @@ import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        {/* Navbar */}
+      </Router>
+    </>
   );
 }
 
