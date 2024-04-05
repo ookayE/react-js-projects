3f46ea153e571d399c2c464bca83c830e3cbc873
ookayE <smith.evan93@gmail.com>
2024-04-05 12:46:55 -0400

    simple tab component. ZERO styling.

--- react-tabs-component/src/App.js
+++ react-tabs-component/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TabButton from "./Components/TabButtons";
+import TabContent from "./Components/TabContent";
 
 function App() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const petData = [
+    {
+      animal: "Cheetah",
+      fact: "Cheetahs are the fastest land animals, capable of reaching speeds up to 75 mph.",
+    },
+    {
+      animal: "Koala",
+      fact: "Koalas sleep around 20 hours a day and are known for their eucalyptus diet.",
+    },
+    {
+      animal: "Elephant",
+      fact: "Elephants have the largest brains among land animals and demonstrate remarkable intelligence.",
+    },
+    {
+      animal: "Zebra",
+      fact: "Zebras have distinctive black and white stripes that act as a natural defense against predators.",
+    },
+    {
+      animal: "Horse",
+      fact: "Horses have excellent memory and are capable of recognizing human emotions.",
+    },
+  ];
+  //"animal" and "fact"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TabButton petData={petData} setActiveTab={setActiveTab} />
+      <TabContent petData={petData} activeTab={activeTab} />
     </div>
   );
 }
