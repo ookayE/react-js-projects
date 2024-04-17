2277f383f841ee2431d2e4eb9d6ec607c593c3fa
ookayE <smith.evan93@gmail.com>
2024-04-17 12:15:46 -0400

    windowResize hook used in App js

--- custom-hooks/src/App.js
+++ custom-hooks/src/App.js
@@ -1,9 +1,14 @@
+import useWindowResize from "./Components/Hooks/useWindowResize";
+
 const App = () => {
+  const { width, height } = useWindowResize();
   return (
     <div>
       <h1>Window Size</h1>
-      <p>Width: </p>
-      <p>Height: </p>
+      <p>Width: {width} </p>
+      <p>Height: {height} </p>
     </div>
   );
 };
+
+export default App;
