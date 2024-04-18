1cac99bd5e6fe347fe018873406d1fcd4232148c
ookayE <smith.evan93@gmail.com>
2024-04-18 17:06:37 -0400

    context added to handle state. Recipes fetched via forkify api. Details page added for whatever recipe you click. Still need to add functionality to favorites page.

--- food-recipe-app/src/Pages/home/Home.jsx
+++ food-recipe-app/src/Pages/home/Home.jsx
@@ -1,5 +1,23 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+import { useContext } from "react";
+import { GlobalContext } from "../../Context/Context";
+import RecipeItem from "../../Components/RecipeItem";
 
-export default Home;
+export default function Home() {
+  const { loading, recipeList } = useContext(GlobalContext);
+
+  if (loading) return <div>LOADING</div>;
+
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing here. Please search
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
