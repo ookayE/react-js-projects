46b439985cf31c54521accc0ae0a632dfd81d49b
ookayE <smith.evan93@gmail.com>
2024-04-18 01:31:27 -0400

    food recipe app started. home, navbar, favorites, details added.

--- food-recipe-app/src/App.js
+++ food-recipe-app/src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/home/Home";
+import Favorites from "./Pages/Favorites/Favorites";
+import Details from "./Pages/Details/Details";
 
 function App() {
   return (
     <div className="App">
-      <div>Hello</div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+          <Route path="/recipe-item/:id" element={<Details />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
