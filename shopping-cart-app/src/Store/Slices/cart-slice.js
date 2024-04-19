521ff4c69070e50c1c585b819369ec781bd610a9
ookayE <smith.evan93@gmail.com>
2024-04-19 18:07:01 -0400

    redux set up with reducers. Home page fetches products for fake store API. Product tiles and cart tiles created.

---
+++ shopping-cart-app/src/Store/Slices/cart-slice.js
@@ -0,0 +1,24 @@
+//import create slice method
+import { createSlice } from "@reduxjs/toolkit";
+
+//initialize initial state
+const initialState = [];
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      console.log(action);
+      state.push(action.payload);
+    },
+    removeFromCart(state, action) {
+      //returns state after filtering through it to remove any matching id of the product we we clicked
+      return state.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+
+//create slice = > give it name, menion intial state, create actions
+export default cartSlice.reducer;
