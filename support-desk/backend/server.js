852267f7d8ce5f90da05cdccc96c684d684d0710
ookayE <smith.evan93@gmail.com>
2024-05-08 14:25:40 -0400

    more dependencies installed- bcript, colors, dotenv, express, mongoose. gitignore added. server working.

--- support-desk/backend/server.js
+++ support-desk/backend/server.js
@@ -1,1 +1,11 @@
-console.log("Working");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (require, response) => {
+  response.json({ message: "Yes" });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
