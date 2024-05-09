13b2bce94c7101f429111ee76771a55157c2bfcc
ookayE <smith.evan93@gmail.com>
2024-05-09 01:23:28 -0400

    userController and userRoutes added

--- support-desk/backend/server.js
+++ support-desk/backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.port || 5000;
 
 const app = express();
 
-app.get("/", (require, response) => {
-  response.json({ message: "Yes" });
-});
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT);
