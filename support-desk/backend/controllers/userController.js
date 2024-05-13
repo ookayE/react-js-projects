e147a012c3aa7d1ff1e9d966277d9a5b4e71713c
ookayE <smith.evan93@gmail.com>
2024-05-12 23:58:57 -0400

    dbjs added to connect to database. userSchema created.

--- support-desk/backend/controllers/userController.js
+++ support-desk/backend/controllers/userController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require("express-async-handler");
 
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+
 // @desc Register a new user
 // @Route       /api/users
 // @access      Public
@@ -11,7 +14,36 @@ const registerUser = asyncHandler(async (request, response) => {
     response.status(400);
     throw new error("Please include all fields");
   }
-  response.send("Register Route");
+
+  //Find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    response.status(400);
+    throw new Error("User already exists.");
+  }
+
+  //Hash password
+  const salt = await bcrypt.getSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    response.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    response.status(400);
+    throw new error("Invalid user data");
+  }
 });
 
 // @Description Login a new user
