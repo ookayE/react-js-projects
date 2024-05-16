429dd83c30bfb91a70b8763d784db6e2230df6c1
ookayE <smith.evan93@gmail.com>
2024-05-16 01:02:42 -0400

    fixed star rating hover functionality.

--- star-rating/src/Components/index.jsx
+++ star-rating/src/Components/index.jsx
@@ -1,42 +1,36 @@
-import { useState } from "react";
 import { FaStar } from "react-icons/fa";
+import { useState } from "react";
 import "./styles.css";
 
 export default function StarRating({ numberOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
-  function handleClick(getCurrentIndex) {
-    // Set the 'rating' state to the index of the clicked star.
-    setRating(getCurrentIndex);
-  }
+  const handleClick = (index) => {
+    setRating(index);
+  };
 
-  function handleMouseEnter(getCurrentIndex) {
-    // Set the 'hover' state to the index of the star when the mouse enters it.
-    setHover(getCurrentIndex);
-  }
+  const handleMouseEnter = (index) => {
+    setHover(index);
+  };
 
-  function handleMouseLeave() {
-    // Reset the 'hover' state to the current 'rating' to restore previous state.
+  const handleMouseLeave = () => {
     setHover(rating);
-  }
+  };
 
   return (
     <div className="star-rating">
-      {/* Create an array of stars based on the 'numberOfStars' prop. */}
-      {/* Use _ to skip over each star created via .map in the array and only receive index */}
       {[...Array(numberOfStars)].map((_, index) => {
-        index++; // Increment the index by 1 (for 1-based indexing).
+        index += 1;
 
-        // Return a 'FaStar' component representing a star with the following props:
         return (
           <FaStar
             key={index}
-            className={index <= (hover || rating) ? "active" : "inactive"} // Set class based on hover and rating.
+            className={index <= (hover || rating) ? "active" : "inactive"}
             onClick={() => handleClick(index)}
-            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseMove={() => handleMouseEnter(index)}
             onMouseLeave={() => handleMouseLeave()}
-            size={40}
+            size={60}
           />
         );
       })}
