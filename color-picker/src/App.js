2316dd8ebcbbb3eec92d5ae282816094d6db4a07
ookayE <smith.evan93@gmail.com>
2024-06-30 00:30:50 -0400

    basic color picker

---
+++ color-picker/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import ColorPicker from "./Components/ColorPicker";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Custom Color Picker</h1>
+        <ColorPicker />
+      </header>
+    </div>
+  );
+}
+
+export default App;
