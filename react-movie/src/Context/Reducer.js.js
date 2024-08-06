9ad2e6f9118aa0847ad44b27892bad395961c142
ookayE <smith.evan93@gmail.com>
2024-08-06 00:17:24 -0400

    add to watchList/watched reducers.

--- react-movie/src/Context/Reducer.js.js
+++ react-movie/src/Context/Reducer.js.js
@@ -1,7 +1,23 @@
+import {
+  ADD_MOVIE_TO_WATCHED,
+  ADD_MOVIE_TO_WATCHLIST,
+} from "../Components/types";
+
 export function Reducer(state, action) {
   switch (action.type) {
-    case "value":
-      return state;
+    case ADD_MOVIE_TO_WATCHLIST:
+      console.log(action);
+      return {
+        ...state,
+        watchList: [action.payload, ...state.watchList],
+      };
+
+    case ADD_MOVIE_TO_WATCHED:
+      console.log(action);
+      return {
+        ...state,
+        watched: [action.payload, ...state.watched],
+      };
 
     default:
       return state;
