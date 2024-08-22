4cd46059c3cf1264d9403cbd7049bd208db3cbbe
ookayE <smith.evan93@gmail.com>
2024-08-22 00:05:50 -0400

    commits

--- reusable-react/src/App.js
+++ reusable-react/src/App.js
@@ -1,5 +1,11 @@
+import PaginationTest from "./Components/Pagination/test";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PaginationTest />
+    </div>
+  );
 }
 
 export default App;
