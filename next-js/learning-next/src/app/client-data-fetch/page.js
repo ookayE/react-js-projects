13ea7376d52cbd3797d97823c5246c1c14e175c5
ookayE <smith.evan93@gmail.com>
2024-08-28 23:43:08 -0400

    fetch functions added to utility file. client-side user fetch component made

--- next-js/learning-next/src/app/client-data-fetch/page.js
+++ next-js/learning-next/src/app/client-data-fetch/page.js
@@ -1,9 +1,34 @@
 "use client";
 
+import { fetchListOfUsersClient } from "@/utilities/fetchUserData";
+import { useEffect, useState } from "react";
+
 export default function ClientSideDataFetching() {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchListOfUsersClient();
+  }, []);
+
+  if (loading)
+    return (
+      <h3 className="font-extrabold text-3xl">Loading users. Please wait.</h3>
+    );
+
   return (
     <div>
-      <h1>Client side data fetching</h1>
+      <h1 className="text-5xl font-bold text-orange-600 p-4">
+        Client side data fetching
+      </h1>
+
+      <ul className="pt-4">
+        {users && users.length > 0
+          ? users.map((userItem) => (
+              <li className="text-cyan-400 pt-2 px-6">{userItem.firstName}</li>
+            ))
+          : null}
+      </ul>
     </div>
   );
 }
