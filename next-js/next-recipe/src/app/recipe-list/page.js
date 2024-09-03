d3a362afba5c6f48c8707e740a6c16809db50d6b
ookayE <smith.evan93@gmail.com>
2024-09-02 21:06:33 -0400

    recipes fetched and displayed with basic styles. details page exists but doesn't display information yet. shadcn ui elements initialized

---
+++ next-js/next-recipe/src/app/recipe-list/page.js
@@ -0,0 +1,17 @@
+import RecipeList from "@/components/recipe-list";
+
+async function fetchListOfRecipes() {
+  try {
+    const response = await fetch("https://dummyjson.com/recipes");
+    const data = await response.json();
+
+    return data?.recipes;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export default async function Recipes() {
+  const recipeList = await fetchListOfRecipes();
+  return <RecipeList recipeList={recipeList} />;
+}
