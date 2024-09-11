76f4e1c5fc3ab70ba8568c859ed03b4ba6429c4a
ookayE <smith.evan93@gmail.com>
2024-09-10 23:48:24 -0400

    recipe details page added. links not working atm

--- next-js/next-recipe/src/app/recipe-list/[details]/page.js
+++ next-js/next-recipe/src/app/recipe-list/[details]/page.js
@@ -1,3 +1,19 @@
-export default async function RecipeDetails() {
-  return <div>Recipe Details Page</div>;
+async function fetchRecipeDetails(currentRecipeId) {
+  try {
+    const response = await fetch(
+      `https://dummyjson.com/recipes/${currentRecipeId}`
+    );
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export default async function RecipeDetails({ params }) {
+  const getRecipeDetails = await fetchRecipeDetails(params?.details);
+
+  return <RecipeDetails getRecipeDetails={getRecipeDetails} />;
 }
