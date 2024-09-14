f0a3b01afcbfe472fc033f4687f3cdd1093658ed
ookayE <smith.evan93@gmail.com>
2024-09-14 01:12:14 -0400

    connected to DB. setting some routes up. working on hooking up POST requests.

---
+++ next-js/next-blog/src/models/blog.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+// Define schema - defining the structure of data we're storing in MongoDB
+const BlogSchema = new mongoose.Schema({
+  title: String,
+  postNumber: Number,
+  description: String,
+});
+
+// Checking if the Blog model is already created (Mongoose caches models)
+// If it's already defined in mongoose.models, it will reuse it
+// Otherwise, it creates a new model with the name 'Blog' based on the BlogSchema
+const blog = mongoose.models.Blog || mongoose.model("Blog", BlogSchema);
+
+export default blog;
