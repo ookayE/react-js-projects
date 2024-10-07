236e87e75c4b166af74d27c02a037c914e0eac06
ookayE <smith.evan93@gmail.com>
2024-10-07 00:58:09 -0400

    db created

--- next-server-actions/src/app/theory/server-page-example/page.js
+++ next-server-actions/src/app/theory/server-page-example/page.js
@@ -1,18 +1,20 @@
+import { fetchListOfProducts } from "@/actions";
+
 async function ServerActionsExample() {
-  async function fetchListOfProducts() {
-    "use server";
-    const response = await fetch("https://dummyjson.com/products");
-    const data = await response.json();
-
-    return data.products;
-  }
-
   const products = await fetchListOfProducts();
   console.log(products);
 
   return (
     <div>
       <h1>Server Actions Example - Server Components</h1>
+
+      <ul>
+        {products && products.length > 0 ? (
+          products.map((item) => <li>{item.title}</li>)
+        ) : (
+          <h2>No</h2>
+        )}
+      </ul>
     </div>
   );
 }
---
+++ next-server-actions/src/app/user-management/page.js
@@ -0,0 +1,4 @@
+function UserManagement() {
+  return <div>UserManagement</div>;
+}
+export default UserManagement;
