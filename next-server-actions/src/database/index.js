7ce1d3e3622189804ba9effe31f09d7a47e9d129
ookayE <smith.evan93@gmail.com>
2024-10-08 23:26:22 -0400

    continued form. check db connection though

--- next-server-actions/src/database/index.js
+++ next-server-actions/src/database/index.js
@@ -4,10 +4,15 @@ const connectToDB = async () => {
   const url =
     "mongodb+srv://ookayE:crVGZcAXDYstKkfv@evancluster.ahnl3t3.mongodb.net/";
 
-  mongoose
-    .connect(url)
-    .then(() => console.log("Working"))
-    .catch((e) => console.log(e));
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true, // Optional, to ensure compatibility
+      useUnifiedTopology: true, // Optional, for better connection management
+    });
+    console.log("MongoDB connection successful");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
 };
 
 export default connectToDB;
