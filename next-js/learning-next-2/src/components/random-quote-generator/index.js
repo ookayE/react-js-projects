cdd6fc113c6eadefb804365f56dd0f091638c59e
ookayE <smith.evan93@gmail.com>
2024-10-13 01:01:48 -0400

    adding weather component

--- next-js/learning-next-2/src/components/random-quote-generator/index.js
+++ next-js/learning-next-2/src/components/random-quote-generator/index.js
@@ -1,30 +1,32 @@
 "use client";
 
 import { useState } from "react";
+
 import { listOfQuotes } from "../../../utilities/quotes";
 
 export default function RandomQuoteGenerator() {
   const [quote, setQuote] = useState("");
 
-  const generateQuote = () => {
+  const handleGenerate = () => {
     const randomIndex = Math.floor(Math.random() * listOfQuotes.length);
+
     setQuote(listOfQuotes[randomIndex]);
   };
 
   return (
     <div>
-      <h1>Random Quote Generator</h1>
+      <h1>Random Quotes Below</h1>
 
       <button
         type="button"
-        onClick={generateQuote}
         className="rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        onClick={handleGenerate}
       >
         Generate Quote
       </button>
 
-      <h3>{quote.text}</h3>
-      <h4>{quote.author}</h4>
+      <p>{quote.text}</p>
+      <p>{quote.author}</p>
     </div>
   );
 }
