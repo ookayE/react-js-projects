f1ed5bf008a990b2c18f8596bbc809673352c7ba
ookayE <smith.evan93@gmail.com>
2024-10-22 17:06:30 -0400

    basic subtitle search works. uses srtParser and fuse

--- spongebob-clip-finder/src/App.js
+++ spongebob-clip-finder/src/App.js
@@ -1,9 +1,9 @@
+import SubtitleSearch from "./components/SubtitleSearch";
+
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1 className="text-green-500">Hello</h1>
-      </div>
+      <SubtitleSearch />
     </div>
   );
 }
