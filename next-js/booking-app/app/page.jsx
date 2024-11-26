9e18e990fcd881189f52dc7ebb9184f10db815b7
ookayE <smith.evan93@gmail.com>
2024-11-25 23:55:42 -0500

    vite pokedex init

--- next-js/booking-app/app/page.jsx
+++ next-js/booking-app/app/page.jsx
@@ -2,12 +2,6 @@ import RoomCard from "@/components/RoomCard";
 import rooms from "@/data/rooms.json";
 import Heading from "@/components/Heading";
 
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-import { useAthState } from "react-firebase-hooksauth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-
 export default function Home() {
   return (
     <>
--- next-js/booking-app/utilities/firebase.js
+++ next-js/booking-app/utilities/firebase.js
@@ -16,6 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFireStore(app);
+const db = getFirestore(app);
 
 export { db };
