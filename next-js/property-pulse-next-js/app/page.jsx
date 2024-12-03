8c5ac131f4d11d5a143a88e572abc7e614234f6a
ookayE <smith.evan93@gmail.com>
2024-12-03 01:06:43 -0500

    spliced list of properties added to home page. loading and not found pages added

--- next-js/property-pulse-next-js/app/page.jsx
+++ next-js/property-pulse-next-js/app/page.jsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import HomePoperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
     <div>
       <Hero />
       <InfoBoxes />
+      <HomePoperties />
     </div>
   );
 };
