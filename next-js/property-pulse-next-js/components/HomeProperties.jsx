0ff3dc950465ccaeca886927a870abd167afcc64
ookayE <smith.evan93@gmail.com>
2024-12-05 01:04:03 -0500

    properties and home page now fetching listings from mongoDB. image added to individual [id] pages

--- next-js/property-pulse-next-js/components/HomeProperties.jsx
+++ next-js/property-pulse-next-js/components/HomeProperties.jsx
@@ -1,9 +1,15 @@
-import properties from "@/properties.json";
 import PropertyCard from "@/components/PropertyCard";
 import Link from "next/link";
+import connectDB from "@/config/db";
+import Property from "@/models/Property";
 
-const HomeProperties = () => {
-  const recentProperties = properties.slice(0, 3);
+const HomeProperties = async () => {
+  await connectDB();
+
+  const recentProperties = await Property.find({})
+    .sort({ createdAt: -1 })
+    .limit(3)
+    .lean();
 
   return (
     <>
@@ -12,12 +18,12 @@ const HomeProperties = () => {
           <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
             Recent Properties
           </h2>
-          {properties.length === 0 ? (
+          {recentProperties.length === 0 ? (
             <p>No Properties foudn</p>
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
               {recentProperties.map((property) => (
-                <PropertyCard key={properties._id} property={property} />
+                <PropertyCard key={recentProperties._id} property={property} />
               ))}
             </div>
           )}
